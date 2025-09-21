d14a5c76871b2144a9a52499ee9b3242dcfb7fe9
Afdul Rohmat <afdulrohmat03@gmail.com>
2025-09-21 14:38:58 +0700

    Initial commit

---
+++ src/main/java/com/example/aop_idempotency_demo/AopIdempotencyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.aop_idempotency_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopIdempotencyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopIdempotencyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aop_idempotency_demo/AopIdempotencyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aop_idempotency_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopIdempotencyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
